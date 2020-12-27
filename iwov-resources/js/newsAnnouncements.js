11b5649ed5ff861eeae9236eb98f98766cb1cf1f
Tan Yi Ming <xinfinityming@gmail.com>
2020-12-27 16:17:39 +0800

    - added scroll to element for url params for news and announcement page

---
+++ iwov-resources/js/newsAnnouncements.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accordionName = urlParams.get('name')
+
+    if(accordionName) {
+        $('#' + accordionName).collapse('show');
+        setTimeout(function() {
+            $('html, body').animate({
+                scrollTop: $('#' + accordionName).offset().top - 150
+            }, 500);
+        },300);
+    }
+});
